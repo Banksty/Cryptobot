a20d850887c8ebd14166b5aaeffd74cc37db8f3f
Banksty <gamingforlifeliongang@gmail.com>
2021-07-20 20:03:56 +1000

    test

--- readdata.py
+++ readdata.py
@@ -5,33 +5,46 @@ import numpy
 import matplotlib.pyplot as plt
 import pandas 
 from datetime import datetime
-#fullfilename = "BTC-AUD tick 2021-07-11T02-27.json"
-fullfilename= "all.json"
-directory = "C:\\cryptodata\\BTC-AUD\\tick\\2021-07-11\\"
-f = open(directory + fullfilename, "r")
-data = []
-for line in f:
-    jsonstr = line[:-1]
-    try:
-        jdata = json.loads(jsonstr)
-    except:
-        print("error")
-    data.append(jdata)
-f.close
-df = pandas.DataFrame(data)
-df["bestAsk"] = pandas.to_numeric(df["bestAsk"])
-df["bestBid"] = pandas.to_numeric(df["bestBid"])
-df["lastPrice"] = pandas.to_numeric(df["lastPrice"])
-dtdata = []
-for x in df["timestamp"]:
-    dt = datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%fZ')
-    dtdata.append(dt)
-df = df.assign(timestampx = dtdata)
+
+def import_data(messageType):
+    data = []
+    fullfilename= "all.json"
+    directory = "C:\\cryptodata\\BTC-AUD\\{}\\2021-07-11\\"
+    directory = directory.format(messageType)
+    f = open(directory + fullfilename, "r")
+    for line in f:
+        jsonstr = line[:-1]
+        try:
+            jdata = json.loads(jsonstr)
+        except:
+            print("error")
+        data.append(jdata)
+    f.close
+    df = pandas.DataFrame(data)
+    dtdata = []
+    for x in df["timestamp"]:
+        dt = datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%fZ')
+        dtdata.append(dt)
+    df = df.assign(timestampx = dtdata)
+    return df
+
+#initliazation 
 pandas.set_option("display.max.columns", None)
-#df.head()
-#print(df[1:5])
-df.plot(x="timestampx", y=["bestAsk", "bestBid", "lastPrice"], style = ".-")
+tradedata = import_data("trade")
+#print(tradedata.head())
+tickdata = import_data("tick")
+#print(tickdata.head())
+tickdata["bestAsk"] = pandas.to_numeric(tickdata["bestAsk"])
+tickdata["bestBid"] = pandas.to_numeric(tickdata["bestBid"])
+tickdata["lastPrice"] = pandas.to_numeric(tickdata["lastPrice"])
+tradedata["price"] = pandas.to_numeric(tradedata["price"])
+print(tradedata["price"])
+# #df.head()
+# #print(df[1:5])
+tickdata.plot(x="timestampx", y=["bestAsk", "bestBid", "lastPrice"], style = ".-")
+tradedata.plot(x="timestampx", y=["price"], style = ".-")
 plt.show()
 #dat["bestAsk"].plot()
 #plt.plot(df["bestBid"])
 #plt.show()
+
