d631003912fe743da8bb5777f7d009f58ef77209
firebird <tristan.nomadic@gmail.com>
2021-07-24 19:16:58 +1000

    compare timestamps between tick and trade

--- sim.py
+++ sim.py
@@ -13,24 +13,32 @@ tradedata = import_data('trade')
 
 # iterates through every row in 'tradedata'
 # i is the index of the row
-for i, row in tradedata.iterrows():
-    # E.g.    
-    a = row['price']
-    a = row['timestampx']    
-    # 3 different ways to get a value for that row 
-    print([i,
-           row['price'],
-           tradedata['price'][i],
-           tradedata.iloc[i]['price']])
+#for i, row in tradedata.iterrows():
+#    # E.g.    
+#    a = row['price']
+#    a = row['timestampx']   
+#    # 3 different ways to get a value for that row 
+#    print([i, row['price']])
+##           tradedata['price'][i],
+##           tradedata.iloc[i]['price']])
     
     
 # iterate over ticks
-#trace = []
-#trade_i = 0
-#for i, row in tickdata.iterrows():
-#    bestBid = row['bestBid']
-#    bestAsk = row['bestAsk']
-#    timestamp = row['timestampx']
+#tradeprice = []
+#tradevolume = []
+trade_i = 0
+tradetimestamp = tradedata['timestampx'][0]
+for i, row in tickdata.iterrows():
+    if i == len(tickdata) - 1:
+        break    
+    bestBid = row['bestBid']
+    bestAsk = row['bestAsk']
+    timestamp = row['timestampx']
+    timestampnext = tickdata['timestampx'][i+1]
+    
+    if (tradetimestamp > timestamp) and (tradetimestamp <= timestampnext):
+        print('trade happend !!!')
+        
 #    midprice = (bestBid + bestAsk) / 2
 #    print(round(midprice,5))
     
