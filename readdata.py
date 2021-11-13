0dde31f645e1934b7105db91ae25825fa6aa2fc4
Banksty <gamingforlifeliongang@gmail.com>
2021-11-13 18:32:39 +1100

    new code

--- readdata.py
+++ readdata.py
@@ -41,12 +41,33 @@ def import_data(messageType):
     elif messageType == 'trade':
         df["price"] = pandas.to_numeric(df["price"])
         df["volume"] = pandas.to_numeric(df["volume"])
+    elif messageType == 'orderbook':
+        bids = []
+        asks = []
+        for i, row in df.iterrows():
+            obids = numpy.array(df['bids'][i]).astype(numpy.float)
+            oasks = numpy.array(df['asks'][i]).astype(numpy.float)
+            bids.append(obids)
+            asks.append(oasks)
+        df = df.assign(bidsx = bids)
+        df = df.assign(asksx = asks)
+
+
+       # df[""] = pandas.to_numeric(df[""])
+       # df[""] = pandas.to_numeric(df[""])
         
     return df
 
 pandas.set_option("display.max.columns", None)
 # tickdata = import_data('tick')
 orderbook = import_data('orderbook')
+
+# for x in orderbook:
+#     obids = numpy.array(orderbook['bids'][x]).astype(numpy.float)
+
+#obids = numpy.array(orderbook['bids'][0]).astype(numpy.float)
+#print(orderbook.iloc[[-1]])  
+#print(orderbook['bidsx'][1])
 # tradedata = import_data('trade')
 # tickdata.plot(x="timestampx", y=["bestAsk", "bestBid", "lastPrice"])
 # tradedata.plot(x="timestampx", y=["price"])
